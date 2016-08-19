e974fafc6b734b68d8bfe33add7378f5fae63439
Indrayani Bhoir <indrayani.bhoir@capgemini.com>
2016-08-19 10:40:28 +0000

    user_service.js edited online with Bitbucket

--- src/main/webapp/static/js/service/user_service.js
+++ src/main/webapp/static/js/service/user_service.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp').factory('UserService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8081/Spring4MVCAngularJSExample/user/';
+    var REST_SERVICE_URI = 'http://localhost:80/Spring4MVCAngularJSExample/user/';
 
     var factory = {
         fetchAllUsers: fetchAllUsers,
